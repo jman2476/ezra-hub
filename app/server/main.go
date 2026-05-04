d7b8425ee08252827ae3da5cb71f25f0750a8c8a
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-05-04 14:26:01 -0400

    add db connection, apicfg to server/main.go

--- app/server/main.go
+++ app/server/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/jman2476/ezra-hub/internal/database"
 	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	db *database.Queries
+}
+
 func main() {
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Error opening database connection: %s", err)
+	}
+
 	port := os.Getenv("SERVER_PORT")
 	const filepathRoot = "."
+
+	apiCfg := apiConfig{
+		db: database.New(db),
+	}
+	log.Printf("apiCfg var: %v", apiCfg)
 
 	mux := http.NewServeMux()
 
