b3e37d195ee7e2e8d5354edab6591c35be2b8028
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-05-06 14:41:44 -0400

    add jwt creation and validation

---
+++ app/server/internal/auth/jwt.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+var malformedHeaderErr = errors.New("Malformed token in header")
+var missingBearerTokenErr = errors.New("Header does not contain token")
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	claims := &jwt.RegisteredClaims{
+		Issuer:    "ezra-hub",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{
+		Issuer: "ezra-hub",
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(t *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("Error parsing token string: %w", err)
+	}
+
+	idString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("Error getting claims subject: %w", err)
+	}
+
+	id, err := uuid.Parse(idString)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("Error parsing id string: %w", err)
+	}
+
+	return id, nil
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,9 @@ require (
 )
 
 require (
+	github.com/alexedwards/argon2id v1.0.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/sys v0.43.0 // indirect
 	golang.org/x/term v0.42.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
