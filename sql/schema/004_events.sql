cf4c29e1fb7a8dd084cb3e24ac811b0ed94326a5
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-05-07 22:02:28 -0400

    add migration for events table, not migrated yet

---
+++ sql/schema/004_events.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+CREATE TABLE events(
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    owner UUID NOT NULL,
+            references users(id)
+            on delete cascade,
+    type TEXT NOT NULL,
+    occurs_on TIMESTAMP NOT NULL,
+    
+);
+
+-- +goose Down
+DROP TABLE events;
