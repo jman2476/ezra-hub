f80970d6e841565d52040889802a1e35b5c7ec03
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-05-11 12:08:48 -0400

    client-side event creation is working

--- app/client/internal/api-caller/newevent_handler.go
+++ app/client/internal/api-caller/newevent_handler.go
@@ -1,17 +1,10 @@
 package apicaller
 
-import (
-	"fmt"
-)
-
 func (c *Client) NewEvent(newEventInfo NewEvent) (Event, error) {
-	resVal, err := c.CreateNewResource(newEventInfo)
+	event, err := CreateNewResource[Event](c, newEventInfo)
 	if err != nil {
 		return Event{}, err
 	}
-	newEvent, ok := resVal.(Event)
-	if !ok {
-		return Event{}, fmt.Errorf("Type Error: response value is not Event struct, it is type %T %T", newEvent, resVal)
-	}
-	return newEvent, nil
+
+	return event, nil
 }
---
+++ app/client/internal/api-caller/newresource_handler.go
@@ -0,0 +1,72 @@
+package apicaller
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type NewResource[Resource any] interface {
+	GetLogName() string
+	GetEndpointURL(*Client) string
+}
+
+func CreateNewResource[R any, NR NewResource[R]](c *Client, newData NR) (R, error) {
+	url := newData.GetEndpointURL(c)
+	var resource R
+
+	resourceData, err := json.Marshal(newData)
+	if err != nil {
+		return resource, fmt.Errorf("Data marshalling error: %w", err)
+	}
+	body := bytes.NewReader(resourceData)
+
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return resource, fmt.Errorf("Error creating request: %w", err)
+	}
+
+	header, ok := c.MakeAuthHeader()
+	if ok {
+		req.Header = header
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return resource, fmt.Errorf("Error executing request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 201 {
+		errResp := struct {
+			Error string `json:"error"`
+		}{}
+
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			return resource, fmt.Errorf("Response code: %s, Error reading response body: %w", res.Status, err)
+		}
+		err = json.Unmarshal(data, &errResp)
+		if err != nil {
+			return resource, fmt.Errorf("Response code: %s, Error reading response body: %w", res.Status, err)
+		}
+
+		return resource, fmt.Errorf("Error creating %s: %s, %s", newData.GetLogName(), res.Status, errResp.Error)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return resource, fmt.Errorf("Error reading response body: %w", err)
+	}
+
+	err = json.Unmarshal(data, &resource)
+	if err != nil {
+		return resource, fmt.Errorf("Error unmarshalling response body: %w", err)
+	}
+
+	fmt.Printf("Created resource of type %T from empty struct %T", resource, nil)
+
+	return resource, nil
+}
