925001fa6e06959b2cb41db97b5be199327e4516
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-05-12 17:02:43 -0400

    add outgoing logs table

---
+++ sql/schema/005_outgoing-logs.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE outgoinglogs(
+    id UUID PRIMARY,
+    encoded_log BYTEA,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    sent BOOLEAN DEFAULT false
+);
+
+
+-- +goose Down
+DROP TABLE outgoinglogs;
