38156d69aef8c63c97a120044f57c785559fe648
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-05-15 14:55:34 -0400

    fix in app/client: print new prompt line after receiving event message

--- app/client/handlers.go
+++ app/client/handlers.go
@@ -12,18 +12,21 @@ import (
 func handlerActiveUsers(cfg *config) func(routing.ActiveUser) msgbroker.AckType {
 	return func(user routing.ActiveUser) msgbroker.AckType {
 		cfg.Term.Write([]byte("\r\n"))
+		defer cfg.termNewLine()
 
 		fmt.Println("\r***************************\r")
 		fmt.Printf("----------%s logged in----------\r\n", user.Name)
 		fmt.Println("***************************\r")
 
-		cfg.termNewLine()
 		return msgbroker.Ack
 	}
 }
 
 func handlerEvent(cfg *config) func(apicaller.Event) msgbroker.AckType {
 	return func(e apicaller.Event) msgbroker.AckType {
+		cfg.Term.Write([]byte("\r\n"))
+		defer cfg.termNewLine()
+
 		fmt.Println("\r************************")
 		fmt.Printf("\rNew %s Event Incoming!\n", strings.ToUpper(e.Category))
 		printEvent(e)
