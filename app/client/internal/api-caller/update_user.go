ea250112da8e19b34f3de2966d34029aee91e259
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-05-24 18:21:23 -0400

    finish client apihandler update user function

--- app/client/internal/api-caller/update_user.go
+++ app/client/internal/api-caller/update_user.go
@@ -1,19 +1,57 @@
 package apicaller
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"reflect"
+	"io"
+	"net/http"
 )
 
 func (c *Client) UpdateUser(nu UserUpdate) (User, error) {
-	v := reflect.ValueOf(nu)
-	fields := v.Fields()
+	url := c.baseURL + "/api/users"
 
-	for data, field := range fields {
-		if field.Interface() != "" {
-			fmt.Printf("\rNew %s: %s\n", data.Name, field.Interface())
-		}
+	updateData, err := json.Marshal(nu)
+	if err != nil {
+		return User{}, fmt.Errorf("Data marshaling error: %w", err)
+	}
+	body := bytes.NewReader(updateData)
+
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return User{}, fmt.Errorf("Error creating request: %w", err)
 	}
 
-	return User{}, nil
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return User{}, fmt.Errorf("Error executing request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return handleStatusError[User](res, "Error updating user data")
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return User{}, fmt.Errorf("Error reading response body: %w", err)
+	}
+
+	var user User
+	err = json.Unmarshal(data, &user)
+	if err != nil {
+		return User{}, fmt.Errorf("Error unmarshaling response body: %w", err)
+	}
+
+	return user, nil
+
+	// v := reflect.ValueOf(nu)
+	// fields := v.Fields()
+
+	// for data, field := range fields {
+	// 	if field.Interface() != "" {
+	// 		fmt.Printf("\rNew %s: %s\n", data.Name, field.Interface())
+	// 	}
+	// }
+
 }
