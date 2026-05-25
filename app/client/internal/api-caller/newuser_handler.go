7936312809cf6a5acb7fd6991446aae17173b694
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-05-25 17:20:46 -0400

    add logging for when user is created before logging in

--- app/client/internal/api-caller/newuser_handler.go
+++ app/client/internal/api-caller/newuser_handler.go
@@ -1,10 +1,14 @@
 package apicaller
 
+import "fmt"
+
 func (c *Client) NewUser(signupInfo NewUser) (User, error) {
 	user, err := CreateNewResource[User](c, signupInfo, false)
 	if err != nil {
 		return User{}, err
 	}
+
+	fmt.Printf("\rNew user %s created", user.Name)
 
 	var login = UserLogin{
 		Name:     user.Name,
