ce93b2f90f0fec3a87088f0c6ef1cc9f84a0cb48
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-05-30 14:05:38 -0400

    switching dbUrl, need to add handling for docker vs dev

--- app/server/main.go
+++ app/server/main.go
@@ -23,14 +23,14 @@ type apiConfig struct {
 
 func main() {
 	godotenv.Load("./app/server/.env")
-	// dbURL := os.Getenv("DB_URL")
+	dbURL := os.Getenv("DB_URL")
 	dbBackup := os.Getenv("DB_URL_BACKUP")
 	platform := os.Getenv("PLATFORM")
 	secret := os.Getenv("SECRET")
 	rabbitConnString := os.Getenv("RABBIT_SERVER")
 	rabbitBackupConn := os.Getenv("RABBIT_SERVER_DOCKER")
 
-	db, err := sql.Open("postgres", dbBackup)
+	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Printf("Error opening database connection: %s", err)
 
