195f117b4fbcc862ceb9b14c987eecd32cd342e9
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-06-08 17:01:11 -0400

    update formatting on client prompts

--- app/client/command_update_event.go
+++ app/client/command_update_event.go
@@ -91,7 +91,7 @@ func commandUpdateEvent(cfg *config) error {
 		eventUpdate.ExpiresAt = eventUpdate.OccursOn.Add(diff)
 	}
 
-	cfg.Term.SetPrompt("Where is the event happening?:\r\n")
+	cfg.Term.SetPrompt("Where is the event happening:\r\n")
 	location, err := cfg.Term.ReadLine()
 	if err != nil {
 		return nil
